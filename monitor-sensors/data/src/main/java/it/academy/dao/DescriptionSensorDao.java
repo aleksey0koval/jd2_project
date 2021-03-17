234f6fff77fc5fdf6f3e1ae3f678573d40815f6c
Aleksey Kovalchuk <aleksey0koval@gmail.com>
2021-03-17 20:12:01 +0300

    Added pagin(not working)

--- monitor-sensors/data/src/main/java/it/academy/dao/DescriptionSensorDao.java
+++
@@ -1,9 +0,0 @@
-package it.academy.dao;
-
-import it.academy.model.sensor.DescriptionSensor;
-import it.academy.model.sensor.Sensor;
-
-public interface DescriptionSensorDao {
-
-    DescriptionSensor getDescSensorById(Integer id);
-}
